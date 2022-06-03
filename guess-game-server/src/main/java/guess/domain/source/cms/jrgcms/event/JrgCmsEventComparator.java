d65ec97883e8f944d54718995f120a1087e4badc
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-06-03 15:09:16 +0300

    Fix Sonar warning

--- guess-game-server/src/main/java/guess/domain/source/cms/jrgcms/event/JrgCmsEventComparator.java
+++ guess-game-server/src/main/java/guess/domain/source/cms/jrgcms/event/JrgCmsEventComparator.java
@@ -5,18 +5,26 @@ import java.util.Comparator;
 public class JrgCmsEventComparator implements Comparator<JrgCmsEvent> {
     private record EventVersionPair(int year, int seasonOrderNumber) {
     }
-    
+
+    private int getSeasonOrderNumber(String season) {
+        if ("Autumn".equals(season)) {
+            return 2;
+        } else {
+            return "Spring".equals(season) ? 1 : 0;
+        }
+    }
+
     private EventVersionPair createEventVersionPair(String eventVersion) {
         String[] strings = eventVersion.split(" ");
-        
+
         if (strings.length < 1) {
             throw new IllegalArgumentException(String.format("Invalid event version: %s", eventVersion));
         }
-        
+
         int year = Integer.parseInt(strings[0]);
         String season = (strings.length > 1) ? strings[1] : null;
-        int seasonOrderNumber = "Autumn".equals(season) ? 2 : ("Spring".equals(season) ? 1 : 0);
-        
+        int seasonOrderNumber = getSeasonOrderNumber(season);
+
         return new EventVersionPair(year, seasonOrderNumber);
     }
 
@@ -24,7 +32,7 @@ public class JrgCmsEventComparator implements Comparator<JrgCmsEvent> {
     public int compare(JrgCmsEvent event1, JrgCmsEvent event2) {
         EventVersionPair eventVersionPair1 = createEventVersionPair(event1.getEventVersion().getIv());
         EventVersionPair eventVersionPair2 = createEventVersionPair(event2.getEventVersion().getIv());
-        
+
         if (eventVersionPair1.year() == eventVersionPair2.year()) {
             return Integer.compare(eventVersionPair1.seasonOrderNumber(), eventVersionPair2.seasonOrderNumber());
         } else {
